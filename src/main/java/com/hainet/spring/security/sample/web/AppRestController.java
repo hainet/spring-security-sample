f7af38d86e6a284556c2c6082379d7a37a3cfb05
hainet <20400616+hainet@users.noreply.github.com>
2018-03-23 11:12:22 +0900

    curlによるサンプルリクエストとプロファイルによる挙動例を記述

--- src/main/java/com/hainet/spring/security/sample/web/AppRestController.java
+++ src/main/java/com/hainet/spring/security/sample/web/AppRestController.java
@@ -8,6 +8,9 @@ import java.security.Principal;
 @RestController
 public class AppRestController {
 
+    // curl localhost:8080/api/credentials -H "Client-Id: id" -H "Cliend-Secret: secret" -v
+    // default -> ヘッダの有無や値に関わらず200を返却。
+    // prod -> ヘッダが無いか値が求められる値と異なる場合401を返却。
     @GetMapping("/api/credentials")
     public Principal credentials(final Principal principal) {
         return principal;
